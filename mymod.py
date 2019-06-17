41c530b711165c118128455d842a4b7931ea5bf5
ydhwa <ydhwa_18@naver.com>
2019-06-17 11:47:12 +0900

    독립실행, import 실행 시 출력 구분

--- mymod.py
+++ mymod.py
@@ -12,3 +12,13 @@ def multiply(a, b):
 
 def devide(a, b):
     return a / b
+
+
+def main():
+    print('최상위 모듈(독립실행)시, 출력됩니다.')
+
+
+if __name__ == '__main__':
+    main()
+else:
+    print('mymod.py의 모듈이름: ', __name__)
